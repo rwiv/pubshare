cabeda644717052faf2e534af574f77a0888eca7
rwiv <zc798546@gmail.com>
2023-09-10 13:24:19 +0900

    feat: add default modules

---
+++ src/dev/dev.controller.ts
@@ -0,0 +1,14 @@
+import { Controller, Get, Logger } from '@nestjs/common';
+
+@Controller('dev')
+export class DevController {
+
+  private readonly logger = new Logger(DevController.name);
+
+  @Get('hello')
+  getHello(): string {
+    this.logger.log('hello');
+    this.logger.debug('logging');
+    return 'hello world';
+  }
+}
